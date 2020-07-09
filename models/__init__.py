6bd1fa81110fe1df4177b4619cf11972f33decf7
gkaln <55789275+gkaln@users.noreply.github.com>
2020-07-09 16:18:00 +0300

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+"""The models subpackage contains definitions for the following model
+architectures: resnet20, resnet56, resnet110
+.. ResNext: https://arxiv.org/abs/1611.05431
+"""
+
+from .resnet import resnet20, resnet56, resnet110
